3b189999a0d8efe6aed3db6df8af733d70388fe6
hsl@shushangyun.com <huangsilin18@dingtalk.com>
2020-11-09 10:16:32 +0800

    【新增】 补充说明

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 "# postgreFramwork" 
+ 组成持久层的组件为 datasource 数据库引擎即jdbc实现为org.postgresql.Driver
+ 连接池 为 alibaba 的Druid
+ orm 框架是 mybaits-plus 
+ 
+1.选择数据库根据实际业务需求来选取
+
+2.Druid是连接池，使用它自然是为了连接数据库的效率和资源有效利用
+
+3.mybatis-plus 是基于mybatis 的再封装，由第三方而非mybatis官方所为，里面提供了遍历的插件和api以供使用
--- src/main/java/cn/ssy/postgre/config/MybatisConfig.java
+++ src/main/java/cn/ssy/postgre/config/MybatisConfig.java
@@ -2,27 +2,29 @@ package cn.ssy.postgre.config;
 
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.MybatisMapWrapperFactory;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author: sl
  * @since: 20/11/05 10:59
  */
 
-@Component
+@Configuration
 @MapperScan("cn.ssy.**.mapper")
 public class MybatisConfig {
 
     /**
      * SQL执行效率插件
      */
-//    @Bean
-//    public PerformanceInterceptor performanceInterceptor() {
-//
-//        return new PerformanceInterceptor();
-//    }
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+
+        return new PerformanceInterceptor();
+    }
+
     /**
      * Map下滑线转驼峰
      *
